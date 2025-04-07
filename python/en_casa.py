c0810858584c3749e05730a0289e5b4c7df77669
cano0102 <canoanderson418@gmail.com>
2025-04-06 22:55:56 -0500

    ejercicio

--- python/en_casa.py
+++ python/en_casa.py
@@ -286,91 +286,181 @@
 
 
 
-bibloteca = []
+# bibloteca = []
 
-def registrar_libros():
-    libros = []
-    nombre = str(input("dame el nombre del libro: "))
-    autor = str(input("dame el autor del libro: "))
-    año = int(input("dame el año del libro: "))
-    precio = int(input("dame el precio del libro: "))
+# def registrar_libros():
+#     libros = []
+#     nombre = str(input("dame el nombre del libro: "))
+#     autor = str(input("dame el autor del libro: "))
+#     año = int(input("dame el año del libro: "))
+#     precio = int(input("dame el precio del libro: "))
 
-    libros.append(nombre)
-    libros.append(autor)
-    libros.append(año)
-    libros.append(precio)
-    bibloteca.append(libros)
+#     libros.append(nombre)
+#     libros.append(autor)
+#     libros.append(año)
+#     libros.append(precio)
+#     bibloteca.append(libros)
     
     
 
-def consultar_libro():
-    nombres = str(input("dame el nombre del libro: "))
-    for libro in bibloteca:
-        if libro[0] == nombres:
-            print("el libro si esta: ",libro[0])
-            break
-    else:
-        print("no existe el libro")
+# def consultar_libro():
+#     nombres = str(input("dame el nombre del libro: "))
+#     for libro in bibloteca:
+#         if libro[0] == nombres:
+#             print("el libro si esta: ",libro[0])
+#             break
+#     else:
+#         print("no existe el libro")
     
 
-def mostrar_todos_los_libros():
-    for libros in bibloteca:
-        print(libros)
+# def mostrar_todos_los_libros():
+#     for libros in bibloteca:
+#         print(libros)
 
 
-def eliminar_libro():
-    nombre_a_eliminar = str(input("dame el nombre del libro a eliminar: "))
-    for libro in bibloteca:
-        if libro[0] == nombre_a_eliminar:
-            bibloteca.remove(libro)
-            print("el libro fue eliminado")
-            break
-    else:
-        print("no existe el libro")
+# def eliminar_libro():
+#     nombre_a_eliminar = str(input("dame el nombre del libro a eliminar: "))
+#     for libro in bibloteca:
+#         if libro[0] == nombre_a_eliminar:
+#             bibloteca.remove(libro)
+#             print("el libro fue eliminado")
+#             break
+#     else:
+#         print("no existe el libro")
 
-def menu():
-    print("----OPCIONES PARA ELEGIR----")
-    print("1.Para registrar libros")
-    print("2.Para consultar libros")
-    print("3. Para eliminar libros")
-    print("4.Para mostrar todos los libros")
-    print("5.Para generar un reporte")
-    print("6.Para salir")
+# def menu():
+#     print("----OPCIONES PARA ELEGIR----")
+#     print("1.Para registrar libros")
+#     print("2.Para consultar libros")
+#     print("3. Para eliminar libros")
+#     print("4.Para mostrar todos los libros")
+#     print("5.Para generar un reporte")
+#     print("6.Para salir")
 
-def generar_reporte():
-    total_de_libros = len(bibloteca)
-    print("esto es el numero de libros:",total_de_libros)
+# def generar_reporte():
+#     total_de_libros = len(bibloteca)
+#     print("esto es el numero de libros:",total_de_libros)
 
-    sumar_libros = (libros for libros in bibloteca)
-    suma = sum(sumar_libros)
-    print("la suma de los libros es:",suma)
+#     sumar_libros = (libros for libros in bibloteca)
+#     suma = sum(sumar_libros)
+#     print("la suma de los libros es:",suma)
     
-    promedio = suma / total_de_libros
-    print("el promedio es:", promedio)
+#     promedio = suma / total_de_libros
+#     print("el promedio es:", promedio)
         
-    precios = (libros[3] for libros in bibloteca)
-    maximo = max(precios)
-    print("el precio mas caro de los libros es:",maximo)
+#     precios = (libros[3] for libros in bibloteca)
+#     maximo = max(precios)
+#     print("el precio mas caro de los libros es:",maximo)
 
 
 
 
 
-while True:
-    menu()
-    opcion = int(input("dame una opcion: "))
+# while True:
+#     menu()
+#     opcion = int(input("dame una opcion: "))
     
-    if opcion == 1:
-        registrar_libros()
-    elif opcion == 2:
-        consultar_libro()
-    elif opcion == 3:
-        eliminar_libro()
-    elif opcion == 4:
-        mostrar_todos_los_libros()
-    elif opcion == 5:
-        generar_reporte()
-    elif opcion == 6:
-        break
-    else:
-        print("opcion no valida")
+#     if opcion == 1:
+#         registrar_libros()
+#     elif opcion == 2:
+#         consultar_libro()
+#     elif opcion == 3:
+#         eliminar_libro()
+#     elif opcion == 4:
+#         mostrar_todos_los_libros()
+#     elif opcion == 5:
+#         generar_reporte()
+#     elif opcion == 6:
+#         break
+#     else:
+#         print("opcion no valida")
+
+
+
+# import random
+# banco = []
+
+# def registrar_usuario():
+#     usuario = {}
+
+#     nombre = str(input("Dame tu nombre: "))
+#     usuario["nombre"] = nombre
+
+#     cuenta = random.randint(10000,20000)
+#     usuario["cuenta"] = cuenta
+
+#     saldo_inicial = float(input("dame tu saldo inicial: "))
+#     usuario["saldo_inicial"] = saldo_inicial
+
+#     banco.append(usuario)
+
+# def menu():
+#     print("---DAME UNA OPCION---")
+#     print("1.Para registrarse")
+#     print("2.Para consultar saldo")
+#     print("3.Para realizar algun movimiento")
+#     print("4.Para ver todos los clientes")
+#     print("5.Para generar un reporte")
+#     print("6.Para salir")
+
+
+# def consultar():
+#     numero_de_cuenta = int(input("Dame el numero de cuenta:"))
+#     for cuenta in banco:
+#         if  numero_de_cuenta == cuenta[1] :
+#             print("tu cuneta si esta")
+#         else:
+#             print("tu cuneta no esta")
+
+# def movimientos():
+#     cuenta_cliente = int(input("Dame el tumero de tu cuenta"))
+#     depositar_o_retirar = int(input("1 para deposirar 2 para retirar: "))
+#     if depositar_o_retirar == 1:
+#         for clientes in banco:
+#             if clientes["cuentas"] == cuenta_cliente:
+#                 depositar = int(input("dame el monto a depositar: "))
+#                 deposito = clientes["saldo_inicial"] + depositar
+#                 clientes.update()
+#                 print(deposito)
+#     elif depositar_o_retirar == 2:
+#         for client in banco:
+#             if client["cuentas"] == cuenta_cliente:
+#                 retiro = int(input("dame el monto a retirar: "))
+#                 retirado  = client["saldo_inicial"] - retiro
+#                 client.update()
+#                 print(retirado)
+        
+
+
+# def mostrar():
+#     for clientes in banco:
+#         print(clientes)
+
+# def reporte():
+#     pass
+
+# while True:
+#     menu()
+#     opcion = int("DAME UNA OPCION: ")
+#     if opcion == 1:
+#         registrar_usuario()
+#     elif opcion == 2:
+#         consultar()
+#     elif opcion == 3:
+#         movimientos()
+#     elif opcion == 4:
+#         mostrar()
+#     elif opcion == 5:
+#         reporte()
+#     elif opcion == 6:
+#         print("gracias")
+#         break
+#     else:
+#         print("opcion invalida")
+
+
+
+
+
+
+
