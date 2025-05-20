b86280db06d54e56c804705c438dea604bca0ef4
cano0102 <canoanderson418@gmail.com>
2025-05-19 22:58:36 -0500

    ejercicio 2

--- javascript/ejercicios_de_clase/ejercicios.js
+++ javascript/ejercicios_de_clase/ejercicios.js
@@ -5,8 +5,34 @@ function suma(num1,num2) {
     
     return num1 + num2
 
+
 }
 
 suma()
 
 
+// 2. Dadas las 3 notas de un aprendiz, calcule la definitiva de la asignatura.
+
+function promedio_notas(notas=0) {
+    
+
+
+    let todas_las_notas = []
+
+    
+    for (let i = 0; i < notas; i++) {
+        
+        let nota = Number(prompt("DAME UNA NOTA:",(i + 1)));
+        todas_las_notas.push(nota)
+
+    }
+
+    let suma = todas_las_notas.reduce((a, b) => a + b, 0)
+    console.log("LA SUMA DE TODAS LAS NOTAS ES " + suma)
+
+
+    let promedio = suma / notas
+
+    console.log( `el promedio de las notas  ${promedio}`)
+        
+}
