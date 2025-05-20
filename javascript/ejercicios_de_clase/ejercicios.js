85d43c19dac53f274f509d4ad3fbc3f8adf87945
cano0102 <canoanderson418@gmail.com>
2025-05-19 23:15:12 -0500

    ejercicio 3

--- javascript/ejercicios_de_clase/ejercicios.js
+++ javascript/ejercicios_de_clase/ejercicios.js
@@ -35,4 +35,29 @@ function promedio_notas(notas=0) {
 
     console.log( `el promedio de las notas  ${promedio}`)
         
+
 }
+
+
+// 3. Dadas las 3 notas de un aprendiz, calcule la definitiva de la asignatura si la primera nota
+// tiene un valor del 20%, la segunda del 30% y la última del 50%.
+
+
+function PromedioDeNotas(num1,num2,num3) {
+    
+    let porcentaje_de_num1 = (num1 * 20) / 100 
+    let porcentaje_de_num2 = (num2 * 30) / 100 
+    let porcentaje_de_num3 = (num3 * 50) / 100 
+
+    let promedio = porcentaje_de_num1 + porcentaje_de_num2 + porcentaje_de_num3
+    
+    
+
+    console.log(`el pormedio de las notas es ${promedio}`)
+    
+}
+
+PromedioDeNotas()
+
+
+
